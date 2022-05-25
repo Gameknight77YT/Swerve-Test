90f8ae5d9e9dfe45f2d7051667dbbccc4eba155c
Gameknight77YT <24jschroeder@gmail.com>
2022-05-25 18:28:21 -0500

    It Works

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,171 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import frc.robot.commands.DefaultDriveCommand;
+import frc.robot.commands.SwerveAutoCommand;
+import frc.robot.subsystems.Drivetrain;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Drivetrain drivetrain = new Drivetrain();
+
+  private final XboxController controller = new XboxController(0);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Set up the default command for the drivetrain.
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    drivetrain.setDefaultCommand(new DefaultDriveCommand(
+            drivetrain,
+            () -> -modifyAxis(controller.getLeftY())/3 * Constants.MAX_VELOCITY_METERS_PER_SECOND,
+            () -> -modifyAxis(controller.getLeftX())/3 * Constants.MAX_VELOCITY_METERS_PER_SECOND,
+            () -> -modifyAxis(controller.getRightX())/2 * Constants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+    ));
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // Back button zeros the gyroscope
+    new Button(controller::getRightBumper)
+            // No requirements because we don't need to interrupt anything
+            .whenPressed(drivetrain::zeroGyroscope);
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // Create config for trajectory
+    TrajectoryConfig config =
+    new TrajectoryConfig(
+        Constants.MAX_VELOCITY_METERS_PER_SECOND/2,
+        Constants.MAX_VELOCITY_METERS_PER_SECOND/3)
+    // Add kinematics to ensure max speed is actually obeyed
+    .setKinematics(Constants.kinematics);
+
+    TrajectoryConfig configRev =
+    new TrajectoryConfig(
+        Constants.MAX_VELOCITY_METERS_PER_SECOND/2,
+        Constants.MAX_VELOCITY_METERS_PER_SECOND/3)
+    // Add kinematics to ensure max speed is actually obeyed
+    .setKinematics(Constants.kinematics).setReversed(true);
+
+    Trajectory exampleTrajectory =
+        TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these  interior waypoints
+        List.of(new Translation2d(1, 0)),
+        // End 
+        new Pose2d(2, 0, new Rotation2d(0)),
+        config);
+
+    Trajectory exampleTrajectory1 =
+        TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(1, 0, new Rotation2d(0)),
+        // Pass through these  interior waypoints
+        List.of(new Translation2d(.5, 0)),
+        // End 
+        new Pose2d(0, 0, new Rotation2d(0)),
+        configRev);
+    
+
+    var thetaController =
+      new ProfiledPIDController(
+      Constants.kPThetaController, 0, 0, Constants.kThetaControllerConstraints);
+      thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    SwerveAutoCommand swerveCommand = new SwerveAutoCommand(
+      exampleTrajectory, 
+      drivetrain::getPose, 
+      Constants.kinematics, 
+      new PIDController(Constants.kPXController, 0, 0), 
+      new PIDController(Constants.kPYController, 0, 0), 
+      thetaController, 
+      0.0, 
+      drivetrain::setModuleStates, 
+      drivetrain);
+
+    SwerveAutoCommand swerveCommand1 = new SwerveAutoCommand(
+      exampleTrajectory1, 
+      drivetrain::getPose, 
+      Constants.kinematics, 
+      new PIDController(Constants.kPXController, 0, 0), 
+      new PIDController(Constants.kPYController, 0, 0), 
+      thetaController, 
+      0.0, 
+      drivetrain::setModuleStates, 
+      drivetrain);
+
+    // Reset odometry to the starting pose of the trajectory.
+    drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // Run path following command, then stop at the end.
+    return swerveCommand
+    .andThen(swerveCommand1)
+    .andThen(() -> drivetrain.stopDrive());
+
+  }
+
+  private static double deadband(double value, double deadband) { 
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
+
+  private static double modifyAxis(double value) {
+    // Deadband
+    value = deadband(value, 0.1);
+
+    // Square the axis
+    value = Math.copySign(value * value, value);
+
+    return value;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,231 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.kauailabs.navx.frc.AHRS;
+import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
+import com.swervedrivespecialties.swervelib.SwerveModule;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import static frc.robot.Constants.*;
+
+public class Drivetrain extends SubsystemBase {
+  
+
+  
+
+  // By default we use a Pigeon for our gyroscope. But if you use another gyroscope, like a NavX, you can change this.
+  // The important thing about how you configure your gyroscope is that rotating the robot counter-clockwise should
+  // cause the angle reading to increase until it wraps back over to zero.
+  //  Remove if you are using a Pigeon
+  //private final PigeonIMU m_pigeon = new PigeonIMU(DRIVETRAIN_PIGEON_ID);
+  //  Uncomment if you are using a NavX
+  private final AHRS navx = new AHRS(Port.kMXP, (byte) 200); // NavX connected over MXP
+  
+   // Odometry class for tracking robot pose
+   SwerveDriveOdometry odometry =
+   new SwerveDriveOdometry(Constants.kinematics, navx.getRotation2d());
+
+   private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+
+   SwerveModuleState[] states =  Constants.kinematics.toSwerveModuleStates(m_chassisSpeeds);;
+
+  // These are our modules. We initialize them in the constructor.
+  private final SwerveModule frontLeftModule;
+  private final SwerveModule frontRightModule;
+  private final SwerveModule backLeftModule;
+  private final SwerveModule backRightModule;
+
+  ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
+
+  public Drivetrain() {
+        new Thread(() -> {
+                try {
+                    Thread.sleep(1000);
+                    zeroGyroscope();
+                } catch (Exception e) {
+                }
+            }).start();
+    
+        
+    // There are 4 methods you can call to create your swerve modules.
+    // The method you use depends on what motors you are using.
+    //
+    // Mk3SwerveModuleHelper.createFalcon500(...)
+    //   Your module has two Falcon 500s on it. One for steering and one for driving.
+    //
+    // Mk3SwerveModuleHelper.createNeo(...)
+    //   Your module has two NEOs on it. One for steering and one for driving.
+    //
+    // Mk3SwerveModuleHelper.createFalcon500Neo(...)
+    //   Your module has a Falcon 500 and a NEO on it. The Falcon 500 is for driving and the NEO is for steering.
+    //
+    // Mk3SwerveModuleHelper.createNeoFalcon500(...)
+    //   Your module has a NEO and a Falcon 500 on it. The NEO is for driving and the Falcon 500 is for steering.
+    //
+    // Similar helpers also exist for Mk4 modules using the Mk4SwerveModuleHelper class.
+
+    // By default we will use Falcon 500s in standard configuration. But if you use a different configuration or motors
+    // you MUST change it. If you do not, your code will crash on startup.
+    // FIXME Setup motor configuration
+    frontLeftModule = Mk4SwerveModuleHelper.createFalcon500(
+            // This parameter is optional, but will allow you to see the current state of the module on the dashboard.
+            tab.getLayout("Front Left Module", BuiltInLayouts.kList)
+                    .withSize(2, 4)
+                    .withPosition(0, 0),
+            // This can either be STANDARD or FAST depending on your gear configuration
+            Mk4SwerveModuleHelper.GearRatio.L1,
+            // This is the ID of the drive motor
+            FRONT_LEFT_MODULE_DRIVE_MOTOR,
+            // This is the ID of the steer motor
+            FRONT_LEFT_MODULE_STEER_MOTOR,
+            // This is the ID of the steer encoder
+            FRONT_LEFT_MODULE_STEER_ENCODER,
+            // This is how much the steer encoder is offset from true zero (In our case, zero is facing straight forward)
+            FRONT_LEFT_MODULE_STEER_OFFSET
+    );
+
+    // We will do the same for the other modules
+    frontRightModule = Mk4SwerveModuleHelper.createFalcon500(
+            tab.getLayout("Front Right Module", BuiltInLayouts.kList)
+                    .withSize(2, 4)
+                    .withPosition(2, 0),
+            Mk4SwerveModuleHelper.GearRatio.L1,
+            FRONT_RIGHT_MODULE_DRIVE_MOTOR,
+            FRONT_RIGHT_MODULE_STEER_MOTOR,
+            FRONT_RIGHT_MODULE_STEER_ENCODER,
+            FRONT_RIGHT_MODULE_STEER_OFFSET
+    );
+
+    backLeftModule = Mk4SwerveModuleHelper.createFalcon500(
+            tab.getLayout("Back Left Module", BuiltInLayouts.kList)
+                    .withSize(2, 4)
+                    .withPosition(4, 0),
+            Mk4SwerveModuleHelper.GearRatio.L1,
+            BACK_LEFT_MODULE_DRIVE_MOTOR,
+            BACK_LEFT_MODULE_STEER_MOTOR,
+            BACK_LEFT_MODULE_STEER_ENCODER,
+            BACK_LEFT_MODULE_STEER_OFFSET
+    );
+
+    backRightModule = Mk4SwerveModuleHelper.createFalcon500(
+            tab.getLayout("Back Right Module", BuiltInLayouts.kList)
+                    .withSize(2, 4)
+                    .withPosition(6, 0),
+            Mk4SwerveModuleHelper.GearRatio.L1,
+            BACK_RIGHT_MODULE_DRIVE_MOTOR,
+            BACK_RIGHT_MODULE_STEER_MOTOR,
+            BACK_RIGHT_MODULE_STEER_ENCODER,
+            BACK_RIGHT_MODULE_STEER_OFFSET
+    );
+  }
+
+  /**
+   * Sets the gyroscope angle to zero. This can be used to set the direction the robot is currently facing to the
+   * 'forwards' direction.
+   */
+  public void zeroGyroscope() {
+    //  Remove if you are using a Pigeon
+   // m_pigeon.setFusedHeading(0.0);
+
+    //  Uncomment if you are using a NavX
+    navx.zeroYaw();
+  }
+
+  public Rotation2d getGyroscopeRotation() {
+    //  Remove if you are using a Pigeon
+    //return Rotation2d.fromDegrees(m_pigeon.getFusedHeading());
+
+    //  Uncomment if you are using a NavX
+//    if (m_navx.isMagnetometerCalibrated()) {
+//      // We will only get valid fused headings if the magnetometer is calibrated
+//      return Rotation2d.fromDegrees(m_navx.getFusedHeading());
+//    }
+
+    // We have to invert the angle of the NavX so that rotating the robot counter-clockwise makes the angle increase.
+    return Rotation2d.fromDegrees(360.0 - navx.getYaw());
+  }
+
+  public void drive(ChassisSpeeds chassisSpeeds) {
+    m_chassisSpeeds = chassisSpeeds;
+    states = Constants.kinematics.toSwerveModuleStates(m_chassisSpeeds);
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
+
+    frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
+    frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
+    backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
+    backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+  }
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return odometry.getPoseMeters();
+  }
+
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    odometry.resetPosition(pose, navx.getRotation2d());
+  }
+
+  /**
+   * Sets the swerve ModuleStates.
+   *
+   * @param states The desired SwerveModule states.
+   */
+  public void setModuleStates(SwerveModuleState[] states) {
+        SwerveDriveKinematics.desaturateWheelSpeeds(
+            states, Constants.MAX_VELOCITY_METERS_PER_SECOND);
+            frontLeftModule.set(states[0].speedMetersPerSecond, states[0].angle.getRadians());
+            frontRightModule.set(states[1].speedMetersPerSecond, states[1].angle.getRadians());
+            backLeftModule.set(states[2].speedMetersPerSecond, states[2].angle.getRadians());
+            backRightModule.set(states[3].speedMetersPerSecond, states[3].angle.getRadians());
+      }
+
+  public void stopDrive(){
+        frontLeftModule.set(0, 0);
+        frontRightModule.set(0, 0);
+        backLeftModule.set(0, 0);
+        backRightModule.set(0, 0);
+  }
+
+
+  @Override
+  public void periodic() {
+    odometry.update(
+        navx.getRotation2d(),
+        states);
+    
+        SmartDashboard.putNumber("gyro Heading", getGyroscopeRotation().getDegrees());
+        SmartDashboard.putNumber("Robot Heading", getPose().getRotation().getDegrees());
+        SmartDashboard.putString("Robot Location", getPose().getX() + ", " + getPose().getY());
+  }
+}
